c698897437968c9ff0e0b85e5cefac6dfe5a9091
YxngXi1 <yang@xue.email>
2024-06-07 13:43:56 -0400

    fixed &apos and &quot in functions and if-statements page

--- app/functions/page.tsx
+++ app/functions/page.tsx
@@ -9,7 +9,7 @@ const page = () => {
         <h1 className='mb-4 '>Functions</h1>
         {/* image will go in here */}
         <div className=' flex  justify-center items-center text-center gap-8 mx-8 '>
-            <h1 className=' w-5/12 '>what is the "fuctnion" of life</h1>
+            <h1 className=' w-5/12 '>what is the &quot;fuctnion&quot; of life</h1>
             <p className=' w-5/12  flex justify-center items-center h-full text-xs'>functions are pretty poggers</p>
         </div>
       </div>
--- app/if-statements/page.tsx
+++ app/if-statements/page.tsx
@@ -9,9 +9,9 @@ const page = () => {
         <h1 className='mb-4 '>If Statement</h1>
         {/* image will go in here */}
         <div className=' flex  justify-center items-center text-center gap-8 mx-8 '>
-            <h1 className=' w-5/12 '>If Macbeth was a good guy he wouldn't have killed Duncan</h1>
-            <p className=' w-5/12  flex justify-center items-center h-full text-xs'>If I was 6"4 would I be him?</p>
-        </div>
+            <h1 className=' w-5/12 '>If Macbeth was a good guy he would&apos;nt have killed Duncan</h1>
+            <p className=' w-5/12  flex justify-center items-center h-full text-xs'>If I was 6&quot;4 would I be him?</p>
+        </div>e
       </div>
       <div>
         <DQuiz />
